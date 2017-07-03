02e1f6bf78911689d8d26ba2111fb90fb6e7e835
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-07-03 09:33:06 +0800

    update docs
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- doc.go
+++ doc.go
@@ -1,25 +1,32 @@
-// Support Let's Encrypt for a Go server application.
-//
-// example for 1-line LetsEncrypt HTTPS servers.
-//
-//    package main
+// Copyright 2017 Bo-Yi Wu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 
-//    import (
-//    	"log"
+/*
+Support Let's Encrypt for a Go server application.
 
-//    	"github.com/gin-gonic/autotls"
-//    	"github.com/gin-gonic/gin"
-//    )
+example for 1-line LetsEncrypt HTTPS servers.
 
-//    func main() {
-//    	r := gin.Default()
+	package main
 
-//    	// Ping handler
-//    	r.GET("/ping", func(c *gin.Context) {
-//    		c.String(200, "pong")
-//    	})
+	import (
+		"log"
 
-//    	log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
-//    }
-//
+		"github.com/gin-gonic/autotls"
+		"github.com/gin-gonic/gin"
+	)
+
+	func main() {
+		r := gin.Default()
+
+		// Ping handler
+		r.GET("/ping", func(c *gin.Context) {
+			c.String(200, "pong")
+		})
+
+		log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
+	}
+
+*/
+
 package autotls
--- example/example1.go
+++ example/example1.go
@@ -1,19 +1,19 @@
-package main
+	package main
 
-import (
-	"log"
+	import (
+		"log"
 
-	"github.com/gin-gonic/autotls"
-	"github.com/gin-gonic/gin"
-)
+		"github.com/gin-gonic/autotls"
+		"github.com/gin-gonic/gin"
+	)
 
-func main() {
-	r := gin.Default()
+	func main() {
+		r := gin.Default()
 
-	// Ping handler
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+		// Ping handler
+		r.GET("/ping", func(c *gin.Context) {
+			c.String(200, "pong")
+		})
 
-	log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
-}
+		log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
+	}
