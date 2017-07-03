e0157e5e37f1af802d5e6222bc14e51692474fac
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-07-03 09:38:25 +0800

    update docs
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- doc.go
+++ doc.go
@@ -1,10 +1,4 @@
-// Copyright 2017 Bo-Yi Wu.  All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-//
 // Support Let's Encrypt for a Go server application.
-//
-// example for 1-line LetsEncrypt HTTPS servers.
 //
 // 	package main
 //
