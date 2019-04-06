8ed28fb848e5f8e5c866294000ce77373e8efd06
Chris Howey <howeyc@gmail.com>
2019-04-05 19:28:17 -0500

    use tlsconfig with additional protos (#11)
    enable tls-alpn ACME challenges

--- autotls.go
+++ autotls.go
@@ -1,7 +1,6 @@
 package autotls
 
 import (
-	"crypto/tls"
 	"net/http"
 
 	"golang.org/x/crypto/acme/autocert"
@@ -16,7 +15,7 @@ func Run(r http.Handler, domain ...string) error {
 func RunWithManager(r http.Handler, m *autocert.Manager) error {
 	s := &http.Server{
 		Addr:      ":https",
-		TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
+		TLSConfig: m.TLSConfig(),
 		Handler:   r,
 	}
 
